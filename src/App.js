ca466df6a72ce0af68be3add3cf3ec8cee8f676f
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-10-18 13:52:03 -0400

    various styling changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header.js";
 import { Route } from 'react-router-dom';
 
+import SearchForm from './components/SearchForm';
 import CharacterList from './components/CharacterList';
 import WelcomePage from './components/WelcomePage';
 
@@ -10,6 +11,7 @@ export default function App() {
     <main>
       <Header />
       <Route exact path='/' component={WelcomePage}/>
+      <SearchForm />
       <Route path='/characterlist' component={CharacterList}/>
       {/* <CharacterList /> */}
     </main>
