e93a618a9057435e805e9555dc0a91a3bf407a80
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-10-18 11:33:41 -0400

    pulled in the data from the API and returned the character list with the list created within CharacterCard

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const CharacterCard = props => {
+  return (
+    <div className='charCard' key={props.index}>
+      <h3>Name: {props.name}</h3>
+      <p>Status: {props.status}</p>
+      <p>Species: {props.species}</p>
+    </div>
+
+  );
 }
+
+export default CharacterCard;
