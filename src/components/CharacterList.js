246a3691775925b6cc77c96980b9c3822eceb0b4
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-10-18 12:54:19 -0400

    got the search to filter through the results and now am working on styling with styled components

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -5,6 +5,7 @@ import CharacterCard from './CharacterCard'
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
@@ -18,10 +19,34 @@ export default function CharacterList() {
       console.log('The data was not retruend', error);
     })
   }, []);
+  
+  useEffect(() => {
+    const characters = data.filter(character => 
+      character.name.toLowerCase().includes(search.toLowerCase())
+    );
+      setData(characters);
+  }, [search]);
+
+  const handleChange = event => {
+    setSearch(event.target.value);
+  }
+
 
   return (
     <section className="character-list">
       <h2>Characters</h2>
+      <div className='character-search'>
+        <form className='search'>
+          <input 
+          type='text'
+          onChange={handleChange}
+          value={search}
+          name='name'
+          placeholder='Search by name'
+          autoComplete='off'
+          />
+        </form>
+      </div>
       {data.map(char => {
         return(
           <CharacterCard 
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,16 +1,24 @@
 import React from "react";
 
+import styled from 'styled-components';
+
+const Header = styled.header`
+  border: 1px solid black;
+  margin: 0 auto;
+  padding: 2%;
+`;
+
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
-      <header>
+      <Header>
         <h1>Welcome to the ultimate fan site!</h1>
         <img
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
         />
-      </header>
+      </Header>
     </section>
   );
 }
